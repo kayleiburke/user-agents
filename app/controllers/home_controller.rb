f51545a72219beac866d5de650c71af3d7ffd392
Kaylei Burke <kaylei.burke@gmail.com>
2019-03-14 12:52:12 -0500

    Display the current user agent string in bold on the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,14 @@
 class HomeController < ApplicationController
   def index
     current_user_agent = request.user_agent
-    user_agent_log = UserAgentLog.find_by(user_agent: current_user_agent)
+    @user_agent_log = UserAgentLog.find_by(user_agent: current_user_agent)
 
-    if user_agent_log
-      count = user_agent_log.count + 1
-      user_agent_log.update!(count: count)
+    if @user_agent_log
+      count = @user_agent_log.count + 1
+      @user_agent_log.update!(count: count)
     else
-      UserAgentLog.create!(user_agent: current_user_agent, count: 1)
+      @user_agent_log = UserAgentLog.new(user_agent: current_user_agent, count: 1)
+      @user_agent_log.save!
     end
 
     @user_agent_logs = UserAgentLog.all.order(:user_agent)
